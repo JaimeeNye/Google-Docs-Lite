b708967adb0fb20da5cbb177f15d19ad6ce41ad2
JaimeeNye <nyejaimee06@gmail.com>
2022-10-02 01:53:59 -0400

    Initial Commit

---
+++ docs-frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  state = { text: '' }
+
+  componentDidMount() {
+    window.fetch('http://localhost:3001/notes/1').then(data => {
+      data.json().then(res => {
+        this.setState({ text: res.text })
+      })
+    })
+  }
+  
+  handleChange = e => {
+    this.setState({ text: e.target.value })
+  }
+
+  render() {
+    return (
+      <textarea
+        value={this.state.text}
+        onChange={this.handleChange}
+      />
+    )
+  }
+}
+
+export default App
